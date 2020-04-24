44cd64f9127ab91706a1336e473f50b2b8eba598
Abhishek Kale <50524422+overrkill@users.noreply.github.com>
2020-04-24 10:37:57 +0530

    changed the idea

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 const quote=document.getElementById('quoter');
+const said=document.getElementById('who');
 const hooman=document.getElementById('human');
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
+
 async function randomQuote() {
+  hooman.src="./assets/load1.gif";
   quote.innerText="a vibe is loading...";
-  var ra=Math.floor(scale(Math.random(),0,1,0,11));
+  said.innerText="unknown";
+
   const response = await fetch('https://api.quotable.io/random?maxLength==9');
   const data = await response.json();
-  console.log(data.content);
-  console.log(ra);
-  hooman.src="./assets/human/"+ra+".png"
+  console.log(data.content,data.author);
+  if (data){hooman.style="display:none";}
   quote.innerText=data.content;
+  console.log(said);
+  said.innerText=data.author;
 
 }
 
